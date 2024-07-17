e7a3fb550733f1170262a7ff21ffc13043cabece
akash-panthri <akashpanthri2000@gmail.com>
2024-07-17 07:03:33 +0530

    Added Code to product.js in routes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
-
+require('express-async-errors')
 const express = require('express')
 const app = express()
 // async error
-const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
+const notFoundMiddleware = require('./middleware/not-found')
 const connectDb = require('./db/connect')
+const routes = require('./routes/products')
+
+
 //middleware
 app.use(express.json())
 
@@ -15,13 +18,13 @@ app.get('/', (req, res)=>{
     
 })
 
-// app.use('/api/v1/products',)
 // Connect to Database
 connectDb()
 
 const port = process.env.PORT || 5000
 // product route
 
+app.use('/api/v1/products',routes)
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
 
