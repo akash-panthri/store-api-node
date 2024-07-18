013bbc5ace592c54dd1f7847ebc0c7aa30a446ce
akash-panthri <akashpanthri2000@gmail.com>
2024-07-18 19:10:41 +0530

    modified route functions in product.js in controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,15 @@
-
+const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res)=>{
-    res.status(200).json({msg:'products testing route'})
+    const products = await Product.find({featured:true})
+    res.status(200).json({products,  nbHits: products.length})
 }
 
 
 
 const getAllProducts = async (req, res)=>{
-    res.status(200).json({msg:'products route'})
+    const products = await Product.find(req.query)
+    res.status(200).json({products,  nbHits: products.length})
 }
 
 module.exports = {getAllProductsStatic, getAllProducts}
